7877cf2067a816f542fa5380c7302650a9d178a0
16sturk-lang <16sturk.stem@gmail.com>
2025-09-26 17:32:28 -0400

    added javascript page with pseudocode instructions

---
+++ script.js
@@ -0,0 +1,8 @@
+// set variables
+
+// on yes click, change to boo-accept page
+
+// on no click, shrink no button
+// iterate on sad ghost image
+// alternate no button text
+// increase yes button size
